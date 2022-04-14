2378cdaf201ae42861138c1e6e33d2dc63eeb980
Rasujon3 <rasujon3@gmail.com>
2022-04-14 18:03:02 +0600

    61-3 Navigate to services, 404 page, Login component

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -14,14 +14,14 @@ const Header = () => {
         variant="dark"
       >
         <Container>
-          <Navbar.Brand as={Link} to="/">
+          <Navbar.Brand as={Link} to="/home">
             <img height={30} src={logo} alt="logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#features">Services</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link href="home#services">Services</Nav.Link>
+              <Nav.Link href="home#experts">Experts</Nav.Link>
               <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
---
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h2 className="text-center text-primary">Machanic is sleeping</h2>
+      <img
+        className="w-100"
+        src="https://genius-car-services-6b155.web.app/static/media/404_NotFound.3be289e9f44d9dd7de7d.jpg"
+        alt="sleeping"
+      />
+    </div>
+  );
+};
+
+export default NotFound;
