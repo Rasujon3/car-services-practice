8485bcfbf4e36bd7e3adf82471e3c3194f02089d
Rasujon3 <rasujon3@gmail.com>
2022-04-14 19:24:41 +0600

    61-4 Style Login form use useRef to access input value

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Button, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    console.log(email, password);
+  };
+
   return (
-    <div>
-      <h2>Please Login</h2>
+    <div className="container w-50 mx-auto">
+      <h2 className="text-primary text-center mt-2">Please Login</h2>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          {/* <Form.Label>Email address</Form.Label> */}
+          <Form.Control
+            ref={emailRef}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          {/* <Form.Label>Password</Form.Label> */}
+          <Form.Control
+            ref={passwordRef}
+            type="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+
+        <Button variant="primary w-50 mx-auto d-block mb-2" type="submit">
+          Login
+        </Button>
+      </Form>
+
+      <p>
+        New to Genious Car?{" "}
+        <Link
+          to="/register"
+          className="text-primary pe-auto text-decoration-none"
+        >
+          Please Register
+        </Link>
+      </p>
+      <p>
+        Forget Password?{" "}
+        <button className="btn btn-link text-primary pe-auto text-decoration-none">
+          Reset Password
+        </button>
+      </p>
     </div>
   );
 };
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Register.css";
 
 const Register = () => {
   return (
-    <div>
-      <h2>Please Register</h2>
+    <div className="register-form">
+      <h2 style={{ textAlign: "center" }}>Please Register</h2>
+      <form>
+        <input type="text" name="name" placeholder="Your Name" />
+        <input type="email" name="email" placeholder="Email Address" required />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+        />
+        <input type="checkbox" name="terms" id="terms" />
+        {/* <label className={agree ? "ps-2" : "ps-2 text-danger"} htmlFor="terms"> */}
+        <label htmlFor="terms">Accept Genious Car Terms & Condition</label>
+        <input
+          className="w-50 mx-auto btn btn-primary mt-2"
+          type="submit"
+          value="Register"
+        />
+      </form>
+      <p>
+        Already have an account?{" "}
+        <Link to="/login" className="text-primary pe-auto text-decoration-none">
+          Please Login
+        </Link>
+      </p>
     </div>
   );
 };
