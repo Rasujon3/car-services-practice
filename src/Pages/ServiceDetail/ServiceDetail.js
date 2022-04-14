773e049bb6d09af6f1f7d8c2cf706eb6d05f0a79
Rasujon3 <rasujon3@gmail.com>
2022-04-14 21:01:54 +0600

    61-8 auth redirect and signout

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const ServiceDetail = () => {
   const [services, setServices] = useState([]);
@@ -28,13 +28,15 @@ const ServiceDetail = () => {
     <div>
       <h2 className="text-center">More details about: {mappint.name}</h2>
       <div className="service">
-        <img className="w-100" src={mappint.img} alt="" />
+        <img height={400} className="w-100" src={mappint.img} alt="" />
         <h2>{mappint.name}</h2>
         <p>Price: {mappint.price}$</p>
         <p>
           <small>{mappint.description}</small>
         </p>
-        <button>Checkout</button>
+        <Link to="/checkout">
+          <button className="btn btn-primary">Proceed Checkout</button>
+        </Link>
       </div>
     </div>
   );
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,16 @@
+import { signOut } from "firebase/auth";
 import React from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../../firebase.init";
 import logo from "../../../images/logo.png";
 
 const Header = () => {
+  const [user] = useAuthState(auth);
+  const handleSignOut = () => {
+    signOut(auth);
+  };
   return (
     <>
       <Navbar
@@ -14,7 +21,7 @@ const Header = () => {
         variant="dark"
       >
         <Container>
-          <Navbar.Brand as={Link} to="/home">
+          <Navbar.Brand as={Link} to="/">
             <img height={30} src={logo} alt="logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -40,9 +47,13 @@ const Header = () => {
               <Nav.Link as={Link} to="about">
                 About
               </Nav.Link>
-              <Nav.Link as={Link} to="login">
-                Login
-              </Nav.Link>
+              {user ? (
+                <Nav.Link onClick={handleSignOut}>Sign Out</Nav.Link>
+              ) : (
+                <Nav.Link as={Link} to="login">
+                  Login
+                </Nav.Link>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
