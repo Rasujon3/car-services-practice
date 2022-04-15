dde92c9239554db7aa219bc7a60f780bf07d870c
Rasujon3 <rasujon3@gmail.com>
2022-04-15 22:47:10 +0600

    62-6 Conditional css class and conditional disabled

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import auth from "../../../firebase.init";
@@ -6,6 +6,7 @@ import "./Register.css";
 import SocialLogin from "../SocialLogin/SocialLogin";
 
 const Register = () => {
+  const [agree, setAgree] = useState(false);
   const [createUserWithEmailAndPassword, user, loading, error] =
     useCreateUserWithEmailAndPassword(auth);
   const navigate = useNavigate();
@@ -17,7 +18,10 @@ const Register = () => {
     const name = event.target.name.value;
     const email = event.target.email.value;
     const password = event.target.password.value;
-    createUserWithEmailAndPassword(email, password);
+    const agree = event.target.terms.checked;
+    if (agree) {
+      createUserWithEmailAndPassword(email, password);
+    }
   };
   return (
     <div className="register-form">
@@ -31,10 +35,18 @@ const Register = () => {
           placeholder="Password"
           required
         />
-        <input type="checkbox" name="terms" id="terms" />
-        {/* <label className={agree ? "ps-2" : "ps-2 text-danger"} htmlFor="terms"> */}
-        <label htmlFor="terms">Accept Genious Car Terms & Condition</label>
         <input
+          onClick={() => setAgree(!agree)}
+          type="checkbox"
+          name="terms"
+          id="terms"
+        />
+        {/* <label className={agree ? "ps-2" : "ps-2 text-danger"} htmlFor="terms"> */}
+        <label className={`ps-2 ${agree ? "" : "text-danger"}`} htmlFor="terms">
+          Accept Genious Car Terms & Conditions
+        </label>
+        <input
+          disabled={!agree}
           className="w-50 mx-auto btn btn-primary mt-2"
           type="submit"
           value="Register"
