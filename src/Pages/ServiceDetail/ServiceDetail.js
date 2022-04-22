a2f1bbdd79c1975a92cad8911090f354abe6df1e
Rasujon3 <rasujon3@gmail.com>
2022-04-22 15:38:15 +0600

    64-4 Load all services

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -2,14 +2,11 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 const ServiceDetail = () => {
-  const [services, setServices] = useState([]);
-  let mappint = {};
+  const [services, setServices] = useState({});
   let { serviceId } = useParams();
 
   useEffect(() => {
-    fetch(
-      "https://raw.githubusercontent.com/ProgrammingHero1/genius-car-service-auth-module-61/main/public/services.json"
-    )
+    fetch(`http://localhost:5000/service/${serviceId}`)
       .then((res) => res.json())
       .then((data) => setServices(data))
       .catch((err) => {
@@ -18,21 +15,16 @@ const ServiceDetail = () => {
       });
   }, [serviceId]);
 
-  for (const service of services) {
-    if (service.id === parseInt(serviceId)) {
-      mappint = service;
-    }
-  }
-
   return (
     <div>
-      <h2 className="text-center">More details about: {mappint.name}</h2>
+      <h1>ID: {serviceId}</h1>
+      <h2 className="text-center">More details about: {services.name}</h2>
       <div className="service">
-        <img height={400} className="w-100" src={mappint.img} alt="" />
-        <h2>{mappint.name}</h2>
-        <p>Price: {mappint.price}$</p>
+        <img height={400} className="w-100" src={services.img} alt="" />
+        <h2>{services.name}</h2>
+        <p>Price: {services.price}$</p>
         <p>
-          <small>{mappint.description}</small>
+          <small>{services.description}</small>
         </p>
         <Link to="/checkout">
           <button className="btn btn-primary">Proceed Checkout</button>
